b85a2889adb917f7d0e1b0046531e814eaf0a021
nati2345 <99682074+nati2345@users.noreply.github.com>
2023-08-22 12:38:44 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,13 +19,5 @@ app.use(errorHandler)
 app.use('/api/recipes', require('./routes/recipeRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
-//serve frontend
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
-
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
-}else{
-    app.get('/' , (req, res) => res.send('please set to production'))
-}
 
 app.listen(port, ()=>console.log(`server started on port ${port}`))
